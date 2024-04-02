967a20432c6ebeebdb8a6aae1eb3eaf2cef787b4
Aurélien <aurelien.strimelle@bstorm.be>
2024-04-02 12:12:20 +0200

    Focus after submit 🤩

--- src/components/search-color/search-color.jsx
+++ src/components/search-color/search-color.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useId } from 'react';
+import { useState, useCallback, useId, useRef } from 'react';
 
 // Composant "SearchColor" qui communique a son parent la couleur rechercher via "onSearch"
 const SearchColor = ({ onSearch }) => {
@@ -8,6 +8,11 @@ const SearchColor = ({ onSearch }) => {
 
     // Variable d'etat (state) pour stocker la valeur du
     const [colorName, setColorName] = useState('');
+
+    // Génération d'une "ref" pour interagir avec l'input (au niveau du DOM)
+    // - Lier avec l'input via l'attibut "ref="
+    // - Interagir avec la balise via la propriété "current"
+    const searchRef = useRef(); 
 
     // Gestion du Submit du formulaire
     const handleSubmit = useCallback((e) => {
@@ -21,6 +26,9 @@ const SearchColor = ({ onSearch }) => {
         //? Reset le formulaire
         setColorName('');
 
+        //? Focus l'input "Intéraction impérative via le DOM"
+        searchRef.current.focus();
+
     }, [colorName]);
 
     // Rendu du composant
@@ -31,6 +39,7 @@ const SearchColor = ({ onSearch }) => {
             </label>
             <div>
                 <input id={formId} type="text" 
+                    ref={searchRef}
                     value={colorName}
                     onChange={(e) => setColorName(e.target.value)} />
                 {' '}
